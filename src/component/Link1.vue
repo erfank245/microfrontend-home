9935dbcf790f2f82582c8c7c8bccc17c4d50095a
erfank245 <erfank245@gmail.com>
2024-09-18 14:52:22 +0330

    update

--- src/component/Link1.vue
+++ src/component/Link1.vue
@@ -1,14 +1,15 @@
-<template>
-    <pre>
-      asndk
-      asdmklaskd
-      samdlkasmd
-      asmdlsakmdl
-      asdm,klsadm
-      asmdlkasmdlkas
-      sadmklsakdl
-      asdkmlaskdl
-    </pre>
+  <template>
+    <div class="paddnav"></div>
+    <div class="center-items">
+      <img 
+        class="logo"
+        src="https://graphicallyapp.ir/MainLogo.png"
+        alt="graphically app"
+      />
+      <div class="text">
+        تست میکرو فرانت اند گرافیکالی
+      </div>
+    </div>
   </template>
   <script>
   export default {
@@ -16,5 +17,24 @@
   };
   </script>
   <style scoped>
-  
+    .paddnav {
+      width: 100%;
+      height: 80px;
+    }
+    .center-items {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      align-items: center;
+      margin: auto;
+      width: 50%;
+    }
+    .logo {
+      width: 100%;
+      max-width: 400px;
+      margin: auto;
+    }
+    .text {
+      font-weight: 600;
+    }
   </style>
